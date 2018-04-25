48aa641d78abf918e9491787988a6f20eb16d3a9
ingDuran <diegoerleyduran@yahoo.com>
2018-04-25 12:19:38 -0500

    Primer commit instanciacion

---
+++ resources/views/home.blade.php
@@ -0,0 +1,57 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Usuarios Registrados</h1>
+@stop
+
+@section('content')
+  <div class="col-sm-12">
+   	<a  style="margin:10px;"  href="{{route('register')}}" class="btn btn-primary pull-left">Nuevo</a>
+
+     <table id="tblusuarios" class="table table-hover table-striped">
+				<thead>
+				<tr>
+					<th width="20px">Consecutivo</th>
+					<th>Nombre </th>
+					<th >&nbsp;</th>
+					<th >&nbsp;</th>
+					<th >&nbsp;</th>
+				</tr>
+		    	</thead>
+				
+				<tbody>
+				  @foreach($users as $user)	
+				<tr>
+					<td>{{ $user->id }}</td>
+					<td>
+						{{ $user->name }}
+					</td>
+					<td width="20px">
+						<a href="" class="btn btn-link">
+							Ver
+						</a>
+					</td>
+					<td width="20px">
+						<a href="" class="btn btn-link">
+							Editar
+						</a>
+					</td>
+					<td width="20px">
+						<a href="" class="btn btn-link">
+							Borrar
+						</a>
+					</td>
+				</tr>
+		     @endforeach
+			</tbody>	
+		</table>
+		
+
+  </div> 
+@stop
+
+@section('js')
+    <script src="{{ asset('js/general.js') }}"> console.log('Hi!'); </script>
+@stop
