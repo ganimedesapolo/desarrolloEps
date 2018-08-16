22d48178145717abfaabf7c0106b9b11d773ef5f
ganimedesapolo <diegoerleyduran@yahoo.com>
2018-08-16 14:51:51 -0500

    leyenda en agregar codigo pais

--- resources/views/paises/partials/form.blade.php
+++ resources/views/paises/partials/form.blade.php
@@ -5,7 +5,7 @@
 	</div>
 	<div class="form-group">
 	  {{ Form::label('codigo', 'Codigo Pais') }}<br>
-	   <span><i>Por favor digite el codigo ISO 3166-1 alfa-2 del pais, puede consultarlo en esta <a href="http://utils.mucattu.com/iso_3166-1.html" target="_blank">tabla.</a></i></span>	
+	   <span><i>Por favor digite el codigo ISO 3166-1 alfa-2 del pais, puede consultarlo en esta <a href="http://utils.mucattu.com/iso_3166-1.html" target="_blank">tabla.</a><br>Digitelo cuidadosamente ya que de esto depende filtrar las ofertas y productos por pais.</i></span>	
 	  {{ Form::text('codigo', null, ['class' => 'form-control', 'id' => 'codigo']) }}
 	</div>
 	
