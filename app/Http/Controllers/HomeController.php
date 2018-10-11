8ae998cce501a1541ffa0481093e0144fbadc13a
ganimedesapolo <diegoerleyduran@yahoo.com>
2018-10-11 17:04:18 -0500

    solo login los admins

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,8 +23,17 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+     
+       $user=auth()->user();
+       $rol = $user->rol;
+        if($rol==0){
+        //cerrar sesion
+        $request->session()->flush();    
+        return redirect('/');
+       }
+
         $users = User::orderBy('id','DESC')->get();
         return view('home',compact('users'));
     }
